a29808be4fc7fb74c0f7f1f0733980fea6d2ac03
huyyeyetha <146353790+huyyeyetha@users.noreply.github.com>
2024-11-04 12:36:04 +0700

    Delete Backend directory

--- Backend/src/routes/garden.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import gardenController from '../controllers/gardenController';
-
-router.get('/', gardenController.getAllGarden);
-
-export default router;
--- Backend/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import userController from '../controllers/userController';
-
-router.post('/login', userController.login);
-router.put('/logout', userController.logout);
-
-export default router;
